212441d9d1e07c395ae80a1badd1c98634a13d95
Monikeleeeee <selvenytmonika@gmail.com>
2025-10-23 01:26:03 +0300

    added correct uri for ui

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("https://plankton-app-wkqvh.ondigitalocean.app/")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
